03c912e5080584ec8a6da1f2b7cbef8ac3aefc91
Rubayat Imran <rubayat120921@gmail.com>
2022-10-20 22:07:38 +0600

    Update 20 10 22

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
+const currentTask = process.env.npm_lifecycle_event
 const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 
 const postCSSPlugins = [
   require('postcss-import'),
@@ -13,28 +17,54 @@ const postCSSPlugins = [
   require('autoprefixer')
 ]
 
-module.exports = {
+let cssConfig = {
+  test: /\.css$/i,
+  use: ["css-loader?url=false", { loader: "postcss-loader", options: { postcssOptions: { plugins: postCSSPlugins } } }]
+}
+
+let config = {
   entry: './app/assets/scripts/App.js',
-  output: {
+  module: {
+    rules: [
+      cssConfig
+    ]
+  }
+}
+
+if (currentTask == 'dev') {
+  cssConfig.use.unshift('style-loader')
+  config.output = {
     filename: 'bundled.js',
     path: path.resolve(__dirname, 'app')
-  },
-  devServer: {
-    before: function(app, server) {
+  }
+  config.devServer = {
+    before: function (app, server) {
       server._watch('./app/**/*.html')
     },
     contentBase: path.join(__dirname, 'app'),
     hot: true,
     port: 3000,
     host: '0.0.0.0'
-  },
-  mode: 'development',
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader?url=false", { loader: "postcss-loader", options: { postcssOptions: { plugins: postCSSPlugins } } }]
-      }
-    ]
   }
+  config.mode = 'development'
 }
+
+if (currentTask == 'build') {
+  cssConfig.use.unshift(MiniCssExtractPlugin.loader)
+  config.output = {
+    filename: '[name].[chunkhash].js',
+    chunkFilename: '[name].[chunkhash].js',
+    path: path.resolve(__dirname, 'dist')
+  }
+  config.mode = 'production'
+
+  config.optimization = {
+    splitChunks: { chunks: 'all', minSize: 1000 },
+    minimize: true,
+    minimizer: [`...`, new CssMinimizerPlugin()]
+  }
+
+  config.plugins = [new CleanWebpackPlugin(), new MiniCssExtractPlugin({ filename: 'styles.[chunkhash].css' })]
+}
+
+module.exports = config
