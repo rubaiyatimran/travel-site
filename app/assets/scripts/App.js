dce49a0b27ca65ed8c8eeefa1dacc39429f52937
Rubayat Imran <rubayat120921@gmail.com>
2022-11-04 19:34:37 +0600

    Completed ut build process configaration.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,7 +3,6 @@ import 'lazysizes'
 import MobileMenu from './modules/MobileMenu'
 import RevealOnScroll from './modules/RevealOnScroll'
 import StickyHeader from './modules/StickyHeader'
-alert("testing!!")
 new StickyHeader()
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75)
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60)
