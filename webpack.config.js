d698943ac1f0bcb8931d0a8388c259270d624f79
Rubayat Imran <rubayat120921@gmail.com>
2022-11-09 22:09:45 +0600

    Update 09 11 22

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const currentTask = process.env.npm_lifecycle_event
 const path = require('path')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
@@ -21,7 +21,7 @@ const postCSSPlugins = [
 
 class RunAfterCompile {
   apply(compiler) {
-    compiler.hooks.done.tap('Copy images', function () {
+    compiler.hooks.done.tap('Copy images', function() {
       fse.copySync('./app/assets/images', './docs/assets/images')
     })
   }
@@ -29,18 +29,18 @@ class RunAfterCompile {
 
 let cssConfig = {
   test: /\.css$/i,
-  use: ["css-loader?url=false", { loader: "postcss-loader", options: { postcssOptions: { plugins: postCSSPlugins } } }]
+  use: ['css-loader?url=false', {loader: 'postcss-loader', options: {postcssOptions: {plugins: postCSSPlugins}}}]
 }
 
-let pages = fse.readdirSync('./app').filter(function (file) {
+let pages = fse.readdirSync('./app').filter(function(file) {
   return file.endsWith('.html')
-
-}).map(function (page) {
+}).map(function(page) {
   return new HtmlWebpackPlugin({
     filename: page,
     template: `./app/${page}`
   })
 })
+
 let config = {
   entry: './app/assets/scripts/App.js',
   plugins: pages,
@@ -58,7 +58,7 @@ if (currentTask == 'dev') {
     path: path.resolve(__dirname, 'app')
   }
   config.devServer = {
-    before: function (app, server) {
+    before: function(app, server) {
       server._watch('./app/**/*.html')
     },
     contentBase: path.join(__dirname, 'app'),
@@ -88,17 +88,15 @@ if (currentTask == 'build') {
     path: path.resolve(__dirname, 'docs')
   }
   config.mode = 'production'
-
   config.optimization = {
-    splitChunks: { chunks: 'all', minSize: 1000 },
+    splitChunks: {chunks: 'all'},
     minimize: true,
     minimizer: [`...`, new CssMinimizerPlugin()]
   }
-
   config.plugins.push(
     new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin({ filename: 'styles.[chunkhash].css' }),
-    new RunAfterCompile()
+    new MiniCssExtractPlugin({filename: 'styles.[chunkhash].css'}),
+    new RunAfterCompile()  
   )
 }
 
