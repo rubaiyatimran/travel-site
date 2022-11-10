18ae46c9905ce31bf9e3fbd7e90a4b238da4407a
Rubayat Imran <rubayat120921@gmail.com>
2022-11-10 20:16:12 +0600

    added client area feature

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,11 +1,10 @@
-exports.handler = function (event, context, callback) {
-  let body;
-
+exports.handler = function(event, context, callback) {
   const secretContent = `
-    <h3>Welcome to the Secret Area.</h3>
-    <p> Imran is watching you</p>
-
-  `;
+  <h3>Welcome To The Secret Area</h3>
+  <p>Here we can tell you that the sky is <strong>blue</strong>, and two plus two equals four.</p>
+  `
+  
+  let body
 
   if (event.body) {
     body = JSON.parse(event.body)
@@ -14,16 +13,15 @@ exports.handler = function (event, context, callback) {
   }
 
   if (body.password == "javascript") {
-
     callback(null, {
       statusCode: 200,
       body: secretContent
     })
-
   } else {
     callback(null, {
       statusCode: 401
     })
   }
 
+  
 }
