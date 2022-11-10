60ca66ae6825314103044c8c7c276d559fd7c65e
Rubayat Imran <rubayat120921@gmail.com>
2022-11-10 19:53:57 +0600

    Updated cloud function to check passward

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,6 +1,29 @@
-exports.handler = function(event, context, callback) {
-  callback(null, {
-    statusCode: 200,
-    body: "Welcome to the super secret area"
-  })
+exports.handler = function (event, context, callback) {
+  let body;
+
+  const secretContent = `
+    <h3>Welcome to the Secret Area.</h3>
+    <p> Imran is watching you</p>
+
+  `;
+
+  if (event.body) {
+    body = JSON.parse(event.body)
+  } else {
+    body = {}
+  }
+
+  if (body.password == "javascript") {
+
+    callback(null, {
+      statusCode: 200,
+      body: secretContent
+    })
+
+  } else {
+    callback(null, {
+      statusCode: 401
+    })
+  }
+
 }
